72f449bfa46117a4a90955cc6033003a71ca0591
aleksandra-zagorovskaya <aleksandrazagorovskaya@gmail.com>
2019-06-08 01:24:00 +0300

    version 1

---
+++ script.js
@@ -0,0 +1,109 @@
+//1
+var str = 'Привет';
+console.log(typeof str);
+
+var sum = 123;
+console.log(typeof sum);
+
+var num = 15.8;
+console.log(typeof num);
+
+var flag = true;
+console.log(typeof flag);
+
+var txt = 'true';
+console.log(typeof txt);
+
+//2
+var a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18;
+a1 = 5%3;
+console.log(a1);
+console.log(typeof a1);
+
+a2 = 3 % 5;
+console.log(a2);//неверно?
+console.log(typeof a2);
+
+a3 = 5 + '3';
+console.log(a3);
+console.log(typeof a3);
+
+a4 = '5'- 3;
+console.log(a4);//неверно?
+console.log(typeof a4);
+
+a5 = 75 + 'кг';
+console.log(a5);
+console.log(typeof a5);
+
+a6 = 785 * 653;
+console.log(a6);
+console.log(typeof a6);
+
+a7 = 100 / 25;
+console.log(a7);
+console.log(typeof a7);
+
+a8 = 0 * 0;
+console.log(a8);
+console.log(typeof a8);
+
+a9 = 0 / 2;
+console.log(a9);
+console.log(typeof a9);
+
+a10 = 89 / 0;
+console.log(a10);
+console.log(typeof a10);
+
+a11= 98 + 2;
+console.log(a11);
+console.log(typeof a11);
+
+a12 = 5 - 98;
+console.log(a12);
+console.log(typeof a12);
+
+a13 = (8 + 56 * 4) / 5;
+console.log(a13);
+console.log(typeof a13);
+
+a14 = (9 - 12) * 7 / (5 + 2);
+console.log(a14);
+console.log(typeof a14);
+
+a15 = + "123";
+console.log(a15);
+console.log(typeof a15);
+
+a16 = 1||0;
+console.log(a16); //что это
+console.log(typeof a16);
+
+a17 = false||true;
+console.log(a17);
+console.log(typeof a17);
+
+a18 = true > 0;
+console.log(a18);
+console.log(typeof a18);
+
+//3
+var str1 = 'Бывало, спит у ног собака,'
+console.log(str1);
+var str2 = 'костер занявшийся гудит,'
+console.log(str2);
+var str3 = 'и женщина из полумрака'
+console.log(str3);
+var str4 = 'глазами зыбкими глядит.'
+console.log(str4);
+var str5 = 'Потом под пихтою приляжет'
+console.log(str5);
+var str6 = 'на куртку рыжую мою'
+console.log(str6);
+var str7 = 'и мне,задумчивая,скажет:'
+console.log(str7);
+var str8 = '«А ну-ка, спой!..» - и я пою.'
+console.log(str8);
+
+
